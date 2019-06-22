ac191863e8115bf5a6234c0ef20dea5233655fa4
weiliangzheng <1067310192@qq.com>
2019-06-22 17:24:20 +0800

    吧Unicode编码的配置文件改成utf-8，然后添加了事务补偿，以及补偿接口

--- portal/src/main/java/com/vivolvle/portal/controller/PortalController.java
+++ portal/src/main/java/com/vivolvle/portal/controller/PortalController.java
@@ -1,12 +1,18 @@
 package com.vivolvle.portal.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.codingapi.tx.annotation.TxTransaction;
+import com.codingapi.tx.framework.utils.SerializerUtils;
+import com.codingapi.tx.model.TransactionInvocation;
+import com.lorne.core.framework.utils.encode.Base64Utils;
 import com.vivolvle.portal.command.TbInventory;
 import com.vivolvle.portal.command.TbOrders;
 import com.vivolvle.portal.config.InventoryClient;
 import com.vivolvle.portal.config.OrderClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -31,5 +37,18 @@ public class PortalController {
         tbInventory.setItemId(1);
         tbInventory.setItemNum(2);
         inventoryClient.update(tbInventory);
+    }
+
+    @PostMapping("notifyResult")
+    public String notifyResult(@RequestBody String jsonStr)
+    {
+        System.out.println("通知地址..." + jsonStr);
+        String data = (String) JSONObject.parseObject(jsonStr).get("json");
+        data = (String) JSONObject.parseObject(data).get("data");
+        byte[] serializers =  Base64Utils.decode(data);
+        TransactionInvocation transactionInvocation = SerializerUtils.parserTransactionInvocation(serializers);
+
+        System.out.println(transactionInvocation.getMethodStr());
+        return null;
     }
 }
